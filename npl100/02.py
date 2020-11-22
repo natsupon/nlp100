0eed45bf315c6064d98c913f313cfe177591c944
natsumiarai <amna.frais.o.07@gmail.com>
2020-11-22 18:33:09 +0900

    change

--- npl100/02.py
+++
@@ -1,7 +0,0 @@
-p = "パトカー"
-t = "タクシー"
-ans = ""
-
-for i in range(0,4):
-    ans += p[i] + t[i]
-print(ans)
--- npl100/03.py
+++
@@ -1,7 +0,0 @@
-import re
-word = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-word = re.sub('[,.]','',word)
-str = word.split()
-
-ans = [len(i) for i in str]
-print(ans)
